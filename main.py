da8f57b873f18a9163da2e93d0baa1b62adf8bb7
Shantanu <om1232050@gmail.com>
2025-02-25 20:05:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,61 @@
 from flask import Flask, render_template, request
+from groq import Groq
+import os
 
 app = Flask(__name__)
 
-def generate_response(soil_ph, history, type, query):
-    ...
+def generate_response(field, weather, history, soil, query):
+    field = input("What stage is your field in? ")
+    weather = input("How have recent weather conditions been? ")
+    history = input("Have your crops experienced anything in the past? ")
+    soil = input("What soil type do you have? ")
+    user_query= input("Ask a question about farming! ")
+    
+    
+    prompt = f"""
+    You are a farming assistant who is going to help farmers answer questions about diseases or help them identify actionable solutions to any other farming related problems that they may be facing.
 
+    Always answer with step by step actionable solutions that the farmer can take.
+    Make the answers as specific and helpful as they can be. Make the answers short and easy to read. Also have a hindi translation of the answer. The hindi should not be too complicated and should contain words only used in normal conversations.
+
+    Answer the following question - '{query}'
+
+    Some information about the farmer's field is given below -
+    The farmer's field stage is {field}
+    The current weather is {weather}
+    Past diseases affecting the crop are {history}
+    The soil type is {soil}
+     """
+def assistant_reply(prompt):
+    client = Groq(
+    api_key = ('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')
+      )
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+        stream=False,
+    )
+
+    print(chat_completion.choices[0].message.content)
+
+assistant_reply(prompt)
+    
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    field1 = request.form.get("field1")
-    field2 = request.form.get("field2")
-    field3 = request.form.get("field3")
-    field4 = request.form.get("field4")
+    field1 = request.form.get("field")
+    field2 = request.form.get("weather")
+    field3 = request.form.get("history")
+    field4 = request.form.get("soil")
     query = request.form.get("query")
 
     response = f"Processed Query: {query} with inputs {field1}, {field2}, {field3}, {field4}"
